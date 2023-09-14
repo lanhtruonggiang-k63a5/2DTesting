85279812d33bc31fe54f20402cd58abdac688ca1
lanhtruonggiang-k63a5 <72454159+lanhtruonggiang-k63a5@users.noreply.github.com>
2023-09-14 10:02:38 +0700

    delete files

---
+++ Assets/Script/TestSizeDelta.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestSizeDelta : MonoBehaviour
+{
+    [SerializeField] RectTransform pictureRT;
+        
+    private void Start() {
+        RectTransform.size
+    }
+}
